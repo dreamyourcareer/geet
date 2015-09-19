85881604822b45861cc81d55b84fc613f17b6586
Navnish Bhardwaj <navnishb@gmail.com>
2015-09-19 16:50:49 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,42 @@
+
+<!-- Footer part start -->
+
+<footer>
+ <div class="container">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 background-container">
+   <div class="row">
+   
+    <div class="col-xs-12 col-sm-12 col-md-7 col-lg-8">
+  <?php dynamic_sidebar( 'sidebar-1' ); ?>
+       </div>
+    
+    <div class="col-xs-12 col-sm-12 col-md-5 col-lg-4 footer-right-block">
+     <article class="contact-block">
+      <h2 class="title">Contact us</h2>
+      <span class="address">110 West A St. #110 San Diego,<br /> CA 92101<br /> 619 338 8335<br /> 619 338 8346<br /> trading@ictcoffee.com</span>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+       <ul class="social-part">
+        <?php echo ict_show_social_icons(); ?>   
+       </ul>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <span class="copyright">Copyright &copy;2015 ICTCoffee</span> 
+      </div>
+     </article>
+     <hr />
+    </div>
+    
+   </div>
+  </div>
+ </div>
+</footer>
+
+<!-- Footer part start -->
+
+
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/bootstrap.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/npm.js"></script>
+</body>
+</html>
